453f99c24f6d6da41cdb62679de296d008865226
jsteinberg4 <jessesteinberg01@gmail.com>
2020-07-12 18:34:31 -0700

    started attempting jquery

--- TuringGamez/TuringGamezView/javascript/GuessTextController.js
+++ TuringGamez/TuringGamezView/javascript/GuessTextController.js
@@ -1,12 +1,19 @@
+$('#songButton').click(function (evt) {
+  console.log('JQuery works')
+});
+
 function getInput() {
   var inpy, output;
-  inpy = document.getElementByID('songChoice').value;
+  inpy = document.getElementById('songChoice').value;
+  console.log('Song choice: ' + inpy)
   output = 0
-  if(typeof(inpy) == "string") {
-  	output = "Searching..."
+
+  if (inpy != "") {
+    output = "Searching..."
   } else {
-  	output = "Invalid input type"
+    output = "Invalid input type"
   }
 
-  document.getElementByID("Choice").innerHTML = output;
+  document.getElementById("Choice").innerHTML = output;
 }
+
